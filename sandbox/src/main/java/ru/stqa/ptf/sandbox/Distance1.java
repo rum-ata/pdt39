5ba111690ea3e0488b87bc9c55126500f31766f9
rum-ata <skv1974@nm.ru>
2017-03-11 17:04:47 +0300

    Класс с расчетом дистанции в одном файле

---
+++ sandbox/src/main/java/ru/stqa/ptf/sandbox/Distance1.java
@@ -0,0 +1,40 @@
+package ru.stqa.ptf.sandbox;
+
+/**
+ * Created by Константин on 11.03.2017.
+ */
+public class Distance1 {
+
+  public static void main (String[] arg){
+
+    Point p1 = new Point(10,20); // экземпляр первой точки
+    Point p2 = new Point(20,30); // экземпляр второй точки
+
+    double d; // объявление переменной дистанции
+    d = distance(p1,p2); // вычисление дистанции
+
+    //вывод рассчитанной дистанции
+    System.out.println("расстояние между точкой Р1 с координатами (" + p1.x + ", " + p1.y + ") и точкой P2 с координатами (" + p2.x + ", "+ p2.y + ")");
+    System.out.println("равно " + d);
+  }
+
+  //функция расчета расстояния между точками на плоскости (корень из суммы квадратов катетов треугольника)
+  public static double distance (Point p1, Point p2){
+    double d;
+    return d = Math.sqrt(( p2.x - p1.x)*( p2.x - p1.x) + (p2.y - p1.y)*( p2.x - p1.x));
+
+
+  }
+
+  //класс Point с координатами точки на плоскости
+  public static class Point {
+
+     double x, y;
+
+    //конструктор
+    public Point(double x, double y) {
+      this.x=x;
+      this.y=y;
+    }
+  }
+}
