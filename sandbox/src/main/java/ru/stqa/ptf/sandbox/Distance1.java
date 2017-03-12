682e5f73423c36b251f209754460e61b49ac414f
rum-ata <skv1974@nm.ru>
2017-03-12 14:53:31 +0300

    Ответ на задание №2

--- sandbox/src/main/java/ru/stqa/ptf/sandbox/Distance1.java
+++ sandbox/src/main/java/ru/stqa/ptf/sandbox/Distance1.java
@@ -10,23 +10,20 @@ public class Distance1 {
     Point p1 = new Point(10,20); // экземпляр первой точки
     Point p2 = new Point(20,30); // экземпляр второй точки
 
-    double d; // объявление переменной дистанции
-    d = distance(p1,p2); // вычисление дистанции
-
     //вывод рассчитанной дистанции
     System.out.println("расстояние между точкой Р1 с координатами (" + p1.x + ", " + p1.y + ") и точкой P2 с координатами (" + p2.x + ", "+ p2.y + ")");
-    System.out.println("равно " + d);
+    System.out.println("равно " + distance(p1,p2));
+
   }
 
   //функция расчета расстояния между точками на плоскости (корень из суммы квадратов катетов треугольника)
   public static double distance (Point p1, Point p2){
+
     double d;
     return d = Math.sqrt(( p2.x - p1.x)*( p2.x - p1.x) + (p2.y - p1.y)*( p2.x - p1.x));
-
-
   }
 
-  //класс Point с координатами точки на плоскости
+  //класс для координат точки на плоскости
   public static class Point {
 
      double x, y;
---
+++ sandbox/src/main/java/ru/stqa/ptf/sandbox/Distance2.java
@@ -0,0 +1,18 @@
+package ru.stqa.ptf.sandbox;
+
+/**
+ * Created by Константин on 12.03.2017.
+ */
+public class Distance2 {
+  public static void main (String[] arg){
+
+    Point p1 = new Point(10,20); // экземпляр первой точки
+    Point p2 = new Point(20,30); // экземпляр второй точки
+
+    //вывод рассчитанной дистанции
+    System.out.println("расстояние между точкой Р1 с координатами (" + p1.x + ", " + p1.y + ") и точкой P2 с координатами (" + p2.x + ", "+ p2.y + ")");
+    System.out.println("равно " + Point.distance(p1,p2));
+
+  }
+
+}
