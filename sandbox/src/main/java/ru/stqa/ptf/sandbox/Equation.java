3517ea5b41782ffda43dbe62c07ae0e4b4e8ff13
rum-ata <skv1974@nm.ru>
2017-04-01 13:35:17 +0300

    первые if из 3 лекции на примере Equation

---
+++ sandbox/src/main/java/ru/stqa/ptf/sandbox/Equation.java
@@ -0,0 +1,33 @@
+package ru.stqa.ptf.sandbox;
+
+/**
+ * Created by Константин on 01.04.2017.
+ */
+public class Equation {
+
+  private int n;
+  private double a;
+  private double b;
+  private double c;
+
+
+  public Equation(double a, double b, double c){
+    this.a = a;
+    this.b = b;
+    this.c = c;
+
+    double d = b*b - 4*a*c;
+    if (d>0) {
+      n=2;
+    } else {
+      if (d == 0){
+        n=1;
+      } else {
+        n=0;
+      }
+    }
+  }
+  public int rootNumber(){
+    return n;
+  }
+}
