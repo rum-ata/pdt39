0050b602039ccd31c83c1d95d5290c13d9d93f01
rum-ata <skv1974@nm.ru>
2017-05-12 22:23:16 +0300

    изменения генератора контакта и теста создания контакта после блока 6_4

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/generators/ContactDataGenerator.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/generators/ContactDataGenerator.java
@@ -41,11 +41,14 @@ public class ContactDataGenerator {
         List<ContactData> contacts = generateContacts (count);
         save (contacts, new File(file));
     }
-
+//{"test1","middle1","last1","nick1","address","tel11","tel12","tel13","email11","email12","email13","test1"});
     private  void save(List<ContactData> contacts, File file) throws IOException {
         Writer writer = new FileWriter(file);
         for (ContactData contact: contacts){
-            writer.write(String.format("%s;%s;%s\n", contact.getName(), contact.getMiddle(), contact.getLastname()));
+            writer.write(String.format("%s;%s;%s;%s;%s;%s;%s;%s;%s;%s;%s;%s;%s\n", contact.getName(), contact.getMiddle(), contact.getLastname(), contact.getNick(),
+                    contact.getAddress(), contact.getHomePhone(),contact.getMobilPhone(),contact.getWorkPhone(),
+                    contact.getEmail(),contact.getEmail2(),contact.getEmail3(),
+                    contact.getPhoto(),contact.getGroup()));
         }
         writer.close();
     }
@@ -54,7 +57,12 @@ public class ContactDataGenerator {
     private  List<ContactData> generateContacts(int count) {
         List<ContactData> contacts = new ArrayList<ContactData>();
         for (int i = 0; i < count; i++){
-            contacts.add(new ContactData().withName((String.format("test %s", i))).withMiddle((String.format("middle %s", i))).withLastname((String.format("last %s", i))));
+            File photo = new File("/src/test/resources/ava.png");
+            contacts.add(new ContactData().withName((String.format("test %s", i))).withMiddle((String.format("middle %s", i))).withLastname((String.format("last %s", i)))
+                    .withNick((String.format("nick %s", i))).withAddress((String.format("address %s", i)))
+                    .withHomePhone((String.format("111 %s", i))).withMobilPhone((String.format("222 %s", i))).withWorkPhone((String.format("222 %s", i)))
+                    .withEmail((String.format("em1 %s", i))).withEmail2((String.format("em2 %s", i))).withEmail3((String.format("em3 %s", i)))
+                    .withPhoto(photo).withGroup("test1"));
         }
         return contacts;
     }
--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
@@ -1,29 +1,42 @@
 package ru.stqa.pft.addressbook.tests;
 
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.ContactData;
 import ru.stqa.pft.addressbook.model.Contacts;
 
 import java.io.File;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 public class ContactCreationTests extends TestBase {
 
-  @Test
+  @DataProvider
+  public Iterator<Object[]> validContact(){
+    List<Object[]> list = new ArrayList<Object[]>();
+    list.add(new Object[]{"test1","middle1","last1","nick1","address","tel11","tel12","tel13","email11","email12","email13","/src/test/resources/ava.png","test1"});
+    list.add(new Object[]{"test1","middle1","last1","nick1","address","tel11","tel12","tel13","email11","email12","email13","/src/test/resources/ava.png","test1"});
+    list.add(new Object[]{"test1","middle1","last1","nick1","address","tel11","tel12","tel13","email11","email12","email13","/src/test/resources/ava.png","test1"});
+    return list.iterator();
+  }
 
-  public void testContactCreation() {
+  @Test (dataProvider = "validContact")
+  public void testContactCreation(String name, String middle, String last, String nick, String address, String hPhone, String mPhone, String wPhone,
+                                  String em1, String em2, String em3, String foto, String gr) {
 
     app.contactC().gotoHomePage();
     Contacts beforeC = app.contactC().allC();
-    File photo = new File("/src/test/resources/ava.png");
-    ContactData contact = new ContactData().withName("test1").withMiddle("middle1").withLastname("last1").withNick("nick1")
-            .withAddress("мой адрес не дом и не улица")
-            .withHomePhone("111").withMobilPhone("222").withWorkPhone("333")
-            .withEmail("q1@q.com").withEmail2("q2@q.com").withEmail3("q3@q.com")
+    File photo = new File(foto);
+    ContactData contact = new ContactData().withName(name).withMiddle(middle).withLastname(last).withNick(nick)
+            .withAddress(address)
+            .withHomePhone(hPhone).withMobilPhone(mPhone).withWorkPhone(wPhone)
+            .withEmail(em1).withEmail2(em2).withEmail3(em3)
             .withPhoto(photo)
-            .withGroup("test1");
+            .withGroup(gr);
 
     app.contactC().createC(contact);
     assertThat(app.contactC().countC(), equalTo(beforeC.size() + 1));
@@ -41,7 +54,6 @@ public class ContactCreationTests extends TestBase {
   }
 
   @Test (enabled = false)
-
   public void testBadContactCreation() {
 
     app.contactC().gotoHomePage();
